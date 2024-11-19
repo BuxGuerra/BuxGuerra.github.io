79c594f33c646b16755bc5ea82531feaf50dda64
BuxGuerra <guilhermebuxguerra@gmail.com>
2024-11-19 11:46:56 -0300

    Criação da função compressor simplificada)

---
+++ compressor.py
@@ -0,0 +1,32 @@
+import trie
+
+def comprimir(arquivoEntrada):
+
+    #inicializar trie
+    dicionario = trie.Trie()
+    for byte in range(256):
+        dicionario.inserir(byte)
+
+    l =""
+
+    #processar arquivo
+    with open(arquivoEntrada, 'rb') as entrada, open("./arquivoSaida", 'wb') as saida:
+    
+        while True:
+            x = entrada.read(1)
+            if not x:
+                break
+
+            if dicionario.procurar(l+x) != None:
+                l = l+x
+            else:
+                saida.write(dicionario.procurar(l).valor)
+                dicionario.inserir(l+x) #garantir que a logica da trie coloca o valor certo
+                l = x
+
+        #Garantir escrita da ultima palavra
+        if l:
+            saida.write(dicionario.procurar(l).valor)
+
+
+    
